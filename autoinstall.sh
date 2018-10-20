16f3650c836eab6450aa32d317abb61c34f2f1f9
Switch <switch@broken.io>
2018-10-20 05:25:06 -0700

    added autoinstall script for puppetmaster, v1

---
+++ autoinstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if !grep -Fxq "deb http://ftp.debian.org/debian jessie-backports main" /etc/apt/sources.list.d/jessie-backports.list ; then
+	echo "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/jessie-backports.list
+fi
+apt-get -y update
+apt-get -y -t jessie-backports install "openjdk-8-jdk-headless"
+apt-get -y install puppetserver
+
+wget https://apt.puppetlabs.com/puppet6-release-jessie.deb
+sudo dpkg -i puppet6-release-jessie.deb
+apt-get -y update
+apt-get -y install puppetserver
+puppetserver ca setup
+
+service start puppetserver
